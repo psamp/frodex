a3b5eac38b633f6a85a18a1620d5b524ebf435d6
Princess Sampson <ps@kingonomics.com>
2015-06-25 19:18:27 -0400

    created service for ig, not working. main.js changes.

--- app/js/frodex/ig.service.js
+++ app/js/frodex/ig.service.js
@@ -4,13 +4,24 @@
 
   angular.module('Frodex')
 
-  .service('IgAPI', ['$http', 
+  .service('IgService', ['$http', 
 
     function ($http) {
 
-      var igSearch = 'https://api.instagram.com/v1/tags/naturalhairstyle/media/recent?client_id=63164feceed04cf39fd05e5230f10e8d';
+      var igEndpoint = 'https://api.instagram.com/v1/tags/naturalhairstyle/media/recent?client_id=63164feceed04cf39fd05e5230f10e8d';
+
+      // get json for instagram search 'naturalhairstyle'
+
+      this.getIgData = function () {
+        
+        $http.get(igEndpoint).success( function (data) {
+          console.log(data);
+        });
+      
+      };
+
 
     }
-  ])
+  ]);
 
 }());
