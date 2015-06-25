bf3006f7d17614c0ca597bc65699c12e13f7d826
Princess Sampson <ps@kingonomics.com>
2015-06-25 17:51:58 -0400

    Style changes, have routes working. Added folders and html files.

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,39 @@
-console.log('Angular Rocks', angular);
+;(function (){
+  
+  'use strict';
+
+  angular.module('Frodex', ['ngRoute'])
+
+  .constant('PARSE', {
+    URL: 'https://api.parse.com/1/',
+    CONFIG: {
+      headers: {
+        'X-Parse-Application-Id' : '3j9jaWVMuEl4gG7wrSsIULpmAzsBbhJzLA9ESI8p',
+        'X-Parse-REST-API-Key' : 'g1JZfytXtixTSJYkNoiJrChQpgGHx00jeLzt5Qm0'
+      }
+    }
+  })
+
+  .config( [ '$routeProvider',
+
+      function ($routeProvider) {
+
+        $routeProvider.when('/codex', {
+          templateUrl: 'js/frodex/codex.tpl.html',
+          controller: ''
+        })
+
+        .when('/about', {
+          templateUrl: 'js/frodex/about.tpl.html',
+          controller: ''
+        })
+
+        .when('/', {
+          templateUrl: 'js/frodex/login.tpl.html',
+          controller: ''
+        });
+      
+    }
+]);
+
+}());
